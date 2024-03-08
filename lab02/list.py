05decb28712df68a3b9d6a367b0b256dfb5dbbe4
MiloszAdamek <miloush00767@gmail.com>
2024-03-08 12:48:45 +0100

    lab02_2

--- lab02/list.py
+++ lab02/list.py
@@ -1,9 +1,9 @@
 class Element():
 
-    def __init__(self, data, next):
-        self.data = data
-        self.next = next
-
+    def __init__(self, data: list):
+        self.data = data #pole data wskazuje na dane
+        self.next = None #pole next wskazuje na następny element
+    
 class LinkedList():
 
     def __init__(self, head=None): #tworzy pustą listę
@@ -13,49 +13,57 @@ class LinkedList():
         self.head = None
 
     def add(self, data):
-        if not self.head:
-            self.head = Element(data)
-            return
-        current = self.head
-        while current.next:
-            current = current.next
-        current.next = Element(data)
+        new_element = Element(data)
+        new_element.next = self.head #ustawia next nowego elementu na bieżącą głowę listy -> nowy element staje się pierwszym elementem listy
+        self.head = new_element #ustawia self.head na nowy element, czyniąc go nową głową listy
         
     def append(self, data):
-        pass
+        new_element = Element(data)
+        if self.is_empty(): 
+            self.head = new_element
+        else:
+            last_element = self.head
+            while last_element.next: #znajdowanie ostatniego elementu listy (aż do elementu którego next = None)
+                last_element = last_element.next  #iteracja po kolejnych elementach
+            last_element.next = new_element #ustawienie next na nowy element -> dodanie nowe element na koniec listy
 
-    def remove(self): #usuwanie elementu z początku listy
-        self.data.remove()
+    def remove(self):
+        if not self.is_empty():
+            first_elem = self.head
+            secound_elem = first_elem.next
+            self.head = secound_elem
 
     def remove_end(self):
-        i = len(self.data)
-        del self.data[i]
+        first_elem = self.head
+        for i in range(self.lenght()):
+            first_elem = first_elem.next
+        first_elem.next = None
+
 
     def is_empty(self):
-        flag = True
-        for i, elem in enumerate(self.data):
-            if elem is None:
-                flag = True
-            else:
-                return False
-        return flag
-    
-    def length(self):
-        current = self.head
-        len = 0
-        while current.next:
-            current = current.next
-            len += 1
-        return len
-    
-    def get(self):
-        return self.data[0]
-    
-    # def __str__(self):
-    #     for elem in self.data:
-    #         print(f"{elem}\n",end='')
+        return self.head == None
 
-        
+    def lenght(self):
+        lenght = 0
+        if not self.is_empty():
+            element = self.head
+            while element:
+                element = element.next
+                lenght += 1
+            return lenght
+
+    def get(self): 
+        first_elem = self.head
+        return first_elem.data
+
+    def __str__(self):
+        list_str = ""
+        current_element = self.head
+        while current_element:
+            list_str += f"-> {current_element.data}\n"
+            current_element = current_element.next
+        return list_str
+
 def main():
     data = [('AGH', 'Kraków', 1919),
             ('UJ', 'Kraków', 1364),
@@ -66,7 +74,14 @@ def main():
     
     uczelnie = LinkedList()
     for i in range(3):
-        uczelnie.add(Element(data[i]))
+        uczelnie.append(data[i])
+    for i in range(3, len(data)):
+        uczelnie.add(data[i])
     print(uczelnie)
+    print(f"List lenght: {uczelnie.lenght()}\n")
+    uczelnie.remove()
+    print(f"First element: {uczelnie.get()}")
+    # uczelnie.remove_end()
+    # print(uczelnie)
 
 main()
