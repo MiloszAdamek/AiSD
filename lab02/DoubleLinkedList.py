ba5ff7d712b3fc8ebd44676d780339e2a374ea62
miloush7 <milosz.adamek@arup.com>
2024-03-13 17:54:51 +0100

    doublelinkedlist

--- lab02/DoubleLinkedList.py
+++ lab02/DoubleLinkedList.py
@@ -3,46 +3,56 @@ class Element():
     def __init__(self, data: list):
         self.data = data #pole data wskazuje na dane
         self.next = None #pole next wskazuje na następny element
+        self.prev = None #pole prev wskazuje na poprzedni element w liście
     
-class LinkedList():
+class DoubleLinkedList():
 
-    def __init__(self, head=None): #tworzy pustą listę
+    def __init__(self, head=None, tail=None): #tworzy pustą listę
         self.head = head #wskazanie na pierwszy element listy
+        self.tail = tail
 
+    #TODO:
     def destroy(self):
-        self.head = None
+        while not self.is_empty():
+            last_element = self.tail
+            
 
-    def add(self, data):
+    #TODO:
+    def add(self, data): #adding to the head
         new_element = Element(data)
+        self.head.prev = new_element #ustawienie prev aktualnego elementu z głowy na nowy element
         new_element.next = self.head #ustawia next nowego elementu na bieżącą głowę listy -> nowy element staje się pierwszym elementem listy
         self.head = new_element #ustawia self.head na nowy element, czyniąc go nową głową listy
         
-    def append(self, data):
+    def append(self, data): #adding to the tail
         new_element = Element(data)
         if self.is_empty(): 
             self.head = new_element
+            self.tail = new_element
         else:
-            last_element = self.head
-            while last_element.next: #znajdowanie ostatniego elementu listy (aż do elementu którego next = None)
-                last_element = last_element.next  #iteracja po kolejnych elementach
-            last_element.next = new_element #ustawienie next na nowy element -> dodanie nowe element na koniec listy
+            last_element = self.tail
+            self.tail.prev = last_element
+            last_element.next = new_element
+            self.tail = new_element
+            
+    # #TODO:
+    # def remove(self):
+    #     if not self.is_empty():
+    #         first_elem = self.head
+    #         secound_elem = first_elem.next
+    #         self.head = secound_elem
 
-    def remove(self):
-        if not self.is_empty():
-            first_elem = self.head
-            secound_elem = first_elem.next
-            self.head = secound_elem
-
-    def remove_end(self):
-        if not self.is_empty():
-            if self.lenght() == 1:
-                self.head = None
-                return
-            last_element = self.head
-            while last_element.next:
-                last_element = last_element.next
-                if last_element.next.next is None:
-                    last_element.next = None
+    # #TODO:
+    # def remove_end(self):
+    #     if not self.is_empty():
+    #         if self.lenght() == 1:
+    #             self.head = None
+    #             return
+    #         last_element = self.head
+    #         while last_element.next:
+    #             last_element = last_element.next
+    #             if last_element.next.next is None:
+    #                 last_element.next = None
 
     def is_empty(self):
         return self.head == None
@@ -76,22 +86,22 @@ def main():
             ('UP', 'Poznań', 1919),
             ('PG', 'Gdańsk', 1945)]
     
-    uczelnie = LinkedList()
+    uczelnie = DoubleLinkedList()
     for i in range(3):
         uczelnie.append(data[i])
     for i in range(3, len(data)):
         uczelnie.add(data[i])
     print(uczelnie)
     print(f"List lenght: {uczelnie.lenght()}\n")
-    uczelnie.remove()
-    print(f"First element: {uczelnie.get()}\n")
-    uczelnie.remove_end()
-    print(uczelnie)
-    uczelnie.destroy()
-    print(uczelnie.is_empty())
-    uczelnie.remove()
-    uczelnie.append(data[0])
-    uczelnie.remove_end()
-    print(uczelnie.is_empty())
+    # uczelnie.remove()
+    # print(f"First element: {uczelnie.get()}\n")
+    # uczelnie.remove_end()
+    # print(uczelnie)
+    # uczelnie.destroy()
+    # print(uczelnie.is_empty())
+    # uczelnie.remove()
+    # uczelnie.append(data[0])
+    # uczelnie.remove_end()
+    # print(uczelnie.is_empty())
 
 main()
