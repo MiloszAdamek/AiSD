4a8d80e82871e8e66227c6e2622158a06643ebfc
MiloszAdamek <miloush00767@gmail.com>
2024-03-24 15:58:34 +0100

    lab4_done

--- lab04/hashTable.py
+++ lab04/hashTable.py
@@ -1,3 +1,12 @@
+class Element:
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+
+    def __str__(self):
+        return f"{self.key}:{self.value}"
+
 class hashTable:
     
     def __init__(self, size, c1=1, c2=0) -> None:
@@ -8,46 +17,68 @@ class hashTable:
 
     def hash(self, key):
         if isinstance(key, str):
-            key = ord(key)
+            key = sum(ord(char) for char in key)
         return key % self.size 
             
-    def collision(self, key, data):
-        hv = self.hash(key)
-        for i in range(self.size):
-            t = (hv + self.c1 * i + self.c2 * i**2) % self.size
-            if self.tab[t] != None:
-                self.tab[t] = data
-        if self.tab[t] is None:
-            print('Brak miejsca')
-            return None
-            
+    def collision(self, hv, n):
+        t = (hv + self.c1 * n + self.c2 * n**2) % self.size
+        return t
+        
     def search(self, key):
-        if self.tab[self.hash(key)] is not None:
-            return self.tab[self.hash(key)]
-        else: return None
-       
-    def insert(self, key, data):
         hv = self.hash(key)
-        if self.tab[hv] is None:
-            self.tab[hv] = data
-        else:
-            hv = self.collision(key, data)
+        n = 0
 
-    def remove(self, key, x=None):
-        self.tab[self.hash(key)] = x
+        while n < self.size:
+            new_hv = self.collision(hv, n)
+            if self.tab[new_hv] is None:
+                return None
+            elif self.tab[new_hv].key == key:
+                return self.tab[new_hv].value
+            n += 1
+        return None      
+     
+    def insert(self, key, data):
+        elem = Element(key, data)
+        hv = self.hash(key)
+        n = 0
+
+        while n < self.size:
+            new_hv = self.collision(hv, n)
+            if self.tab[new_hv] is None:
+                self.tab[new_hv] = elem
+                return 
+            elif self.tab[new_hv].key == key:
+                self.tab[new_hv] = elem #nadpisanie wartości
+                return
+            n += 1
+        print('Brak miejsca')
+    
+    def remove(self, key):
+        hv = self.hash(key)
+        n = 0
+        while n < self.size:
+            new_hv = self.collision(hv, n)
+            if self.tab[new_hv] is None:
+                print("Brak danej")
+                return
+            elif self.tab[new_hv].key == key:
+                self.tab[new_hv] = None
+                return
+            n += 1
 
     def __str__(self):
         result = "{"
-        for i in range(self.size):
-            if self.tab[i] is not None:
-                result += f"{i}: {self.tab[i]}, "
+        for elem in self.tab:
+            result += str(elem)
+            result += ", "
         if result.endswith(", "):
             result = result[:-2]
         result += "}"
         return result
 
-def test1():
-    list_dict = hashTable(size=13)
+def test1(c1, c2):
+    list_dict = hashTable(size=13, c1=c1, c2=c2)
+    print(list_dict)
     letter = 'A'
     for i in range(1, 16):
         if i == 6: i = 18
@@ -63,14 +94,30 @@ def test1():
     list_dict.remove(5)
     print(list_dict)
     print(list_dict.search(31))
+    
+    list_dict.insert('test', 'W')
+    print(list_dict)
 
-def test2():
-    pass
+def test2(c1, c2):
+    list_dict = hashTable(size=13, c1=c1, c2=c2)
+    letter = 'A'
+    for i in range(1, 16):
+        list_dict.insert(13*i,letter)
+        letter = ord(letter) + 1
+        letter = chr(letter)
+    list_dict.insert(5, 'Z')
+    list_dict.remove(5)
+    list_dict.insert('test', 'W')
+    print(list_dict)
 
 def main():
-    test1()
-    test2()
-
+    test1(c1=1, c2=0)
+    print('----------------------------------------------')
+    test2(c1=1, c2=0)
+    print('----------------------------------------------')
+    test2(c1=0, c2=1)
+    print('----------------------------------------------')
+    test1(c1=0, c2=1)
 
 main()
 
