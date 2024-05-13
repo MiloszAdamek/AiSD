9bcbb8bc989875d995636f975965f5bfaffa01f1
MiloszAdamek <miloush00767@gmail.com>
2024-05-13 19:55:57 +0200

    lab10

--- lab10/flow.py
+++ lab10/flow.py
@@ -21,11 +21,11 @@ class Edge:
         self.capacity = capacity #pojemność/przepustowość
         self.isResidual = isResidual #flaga
         if self.isResidual:
+            self.residual = 0
+            self.flow = 0 
+        else:
             self.residual = self.capacity
             self.flow = 0
-        else:
-            self.residual = 0
-            self.flow = 0  
     
     def __repr__(self) -> str:
         str = ""
@@ -87,9 +87,9 @@ class AdjacencyList:
         queue.append(first)
 
         while queue:
-            elem = queue[0]
-            for neighbour in self.neighbours(elem):
-                if neighbour not in visited and self.vertex_dict[elem][neighbour].residual > 0:
+            elem = queue.pop(0)
+            for neighbour, edge in self.neighbours(elem):
+                if neighbour not in visited and edge.residual > 0:
                     visited.add(neighbour)
                     parent[neighbour] = elem
                     queue.append(neighbour)
@@ -97,48 +97,45 @@ class AdjacencyList:
     
     def min_capacity(self, parent: dict, first=None, last=None):
 
-        curr_vertex = first
+        curr_vertex = last
         min_residual = np.inf
-
-        if curr_vertex not in parent.keys():
+        if curr_vertex not in parent:
             return 0
-        else: 
-            while curr_vertex != last:
-                edge = self.vertex_dict[curr_vertex][parent[curr_vertex]]
-                if edge.residual < min_residual:
-                    min_residual = edge.residual
-                curr_vertex = parent[curr_vertex]
+
+        while curr_vertex is not first:
+            edge = self.vertex_dict[parent[curr_vertex]][curr_vertex]
+            if edge.residual < min_residual:
+                min_residual = edge.residual
+            curr_vertex = parent[curr_vertex]
+        
         return min_residual
     
+    def getEdge(self, v1, v2):
+        for edge in self.neighbours(v1):
+            if edge[0] == v2:
+                return edge[1]
+            
     def augumenting_path(self, parent:dict, min_capacity, first, last):
-        curr_vertex = first
+        curr_vertex = last
 
         if curr_vertex not in parent.keys():
             return 0
-        else: 
-            while curr_vertex != last:
-                edge1 = self.vertex_dict[curr_vertex][parent[curr_vertex]]
-                edge2 = self.vertex_dict[parent[curr_vertex]][curr_vertex]
+        
+        while curr_vertex != first:
 
-                if edge1.isResidual:
-                    edge1.flow += min_capacity
-                    edge1.residual -= min_capacity
-                    edge2.residual += min_capacity
-                else:
-                    edge1.residual -= min_capacity
-                    edge2.flow -= min_capacity
-                    edge2.residual += min_capacity
+            edge1 = self.vertex_dict[parent[curr_vertex]][curr_vertex]
+            edge2 = self.vertex_dict[curr_vertex][parent[curr_vertex]]
 
-                if edge2.isResidual:
-                    edge2.flow += min_capacity
-                    edge2.residual -= min_capacity
-                    edge1.residual += min_capacity
-                else:
-                    edge2.residual -= min_capacity
-                    edge1.flow -= min_capacity
-                    edge1.residual += min_capacity
+            if edge1.isResidual:
+                edge1.flow += min_capacity
+                edge1.residual -= min_capacity
+                edge2.residual += min_capacity
+            else:
+                edge1.residual -= min_capacity
+                edge2.flow -= min_capacity
+                edge2.residual += min_capacity
 
-                curr_vertex = parent[curr_vertex]
+            curr_vertex = parent[curr_vertex]
 
     def ford_fulkerson(self):
         first = Vertex('s')
@@ -152,7 +149,7 @@ class AdjacencyList:
             result += min_capacity
             self.augumenting_path(parent, min_capacity, first, last)
             parent = self.BFS(first)
-            self.min_capacity(parent, first, last)
+            min_capacity = self.min_capacity(parent, first, last)
         return result
         
 def printGraph(g):
@@ -166,27 +163,36 @@ def printGraph(g):
     print("-------------------")
 
 def main():
-    graf_0 = [ ('s','u',2), ('u','t',1), ('u','v',3), ('s','v',1), ('v','t',2)]
-    g0 = AdjacencyList()
+    graf_0 = [('s', 'u', 2), ('u', 't', 1), ('u', 'v', 3), ('s', 'v', 1), ('v', 't', 2)]
+    graf_1 = [ ('s', 'a', 16), ('s', 'c', 13), ('c', 'a', 4), ('a', 'b', 12), ('b', 'c', 9), ('b', 't', 20), ('c', 'd', 14), ('d', 'b', 7), ('d', 't', 4) ]
+    graf_2 = [ ('s', 'a', 3), ('s', 'c', 3), ('a', 'b', 4), ('b', 's', 3), ('b', 'c', 1), ('b', 'd', 2), ('c', 'e', 6), ('c', 'd', 2), ('d', 't', 1), ('e', 't', 9)]
 
-    for elem in graf_0:
-        x1, x2, weight = elem
-
-        v1 = Vertex(x1)
-        v2 = Vertex(x2)
-        g0.insert_vertex(v1)
-        g0.insert_vertex(v2)
-
-        edge12 = Edge(weight, isResidual=False)
-        edge21 = Edge(isResidual=True)
-
-        g0.insert_edge(v1, v2, edge12)
-        g0.insert_edge(v2, v1, edge21)
     
-    printGraph(g0)
-    flow = g0.ford_fulkerson()
-    printGraph(g0)
-    print(flow)
+    def run(graf):
+
+        g = AdjacencyList()
+
+        for elem in graf:
+            x1, x2, weight = elem
+
+            v1 = Vertex(x1)
+            v2 = Vertex(x2)
+            g.insert_vertex(v1)
+            g.insert_vertex(v2)
+
+            edge12 = Edge(weight, isResidual=False)
+            edge21 = Edge(isResidual=True)
+
+            g.insert_edge(v1, v2, edge12)
+            g.insert_edge(v2, v1, edge21)
+    
+        flow = g.ford_fulkerson()
+        printGraph(g)
+        print(flow)
+
+    run(graf_0)
+    run(graf_1)
+    run(graf_2)
 
 main()
 
