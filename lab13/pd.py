7728926415c870d9bee5e8934fcad9d48899944d
MiloszAdamek <miloush00767@gmail.com>
2024-06-07 10:48:28 +0200

    lab13

--- lab13/pd.py
+++ lab13/pd.py
@@ -16,24 +16,25 @@ def string_compare_recur(P: str, T: str, i: int, j:int):
 
 def string_compare_PD(P: str, T: str, i: int, j: int):
 
-    D = np.zeros((len(P) + 1, len(T) + 1)).astype('int')
-    for x in range(len(T) + 1):
+    D = np.zeros((len(P), len(T))).astype('int')
+    for x in range(len(T)):
         D[0][x] = x
-    for y in range(len(P) + 1):
+    for y in range(len(P)):
         D[y][0] = y
 
-    Parrents = np.chararray((len(P) + 1, len(T) + 1))
-    for k in range(len(P) + 1):
-        for l in range(len(T) + 1):
-            Parrents[k][l] = 'X'
-    for k in range(1, len(P) + 1):
-        Parrents[k][0] = 'D'
-    for l in range(1, len(T) + 1):
-        Parrents[0][l] = 'I'
+    Parents = np.chararray((len(P), len(T)))
+    for k in range(len(P)):
+        for l in range(len(T)):
+            Parents[k][l] = 'X'
+    Parents = Parents.astype("U256")
+    for k in range(1, len(P)):
+        Parents[k][0] = 'D'
+    for l in range(1, len(T)):
+        Parents[0][l] = 'I'
 
     for k in range(1, i + 1):
         for l in range(1, j + 1):
-            zamian = D[k-1][l-1] + int(P[k-1] != T[l-1])
+            zamian = D[k-1][l-1] + int(P[k] != T[l])
             wstawień = D[k][l-1] + 1
             usunięć = D[k-1][l] + 1
 
@@ -41,21 +42,21 @@ def string_compare_PD(P: str, T: str, i: int, j: int):
             D[k][l] = min_cost
 
             if zamian <= wstawień and zamian <= usunięć:
-                if P[k-1] == T[l-1]:
-                    Parrents[k][l] = 'M'
+                if P[k] == T[l]:
+                    Parents[k][l] = 'M'
                 else:
-                    Parrents[k][l] = 'S'
+                    Parents[k][l] = 'S'
             elif wstawień < zamian and wstawień < usunięć:
-                Parrents[k][l] = 'I'
+                Parents[k][l] = 'I'
             else:
-                Parrents[k][l] = 'D'
+                Parents[k][l] = 'D'
 
-    return D[i][j], Parrents
+    return D[i][j], Parents
 
 def path_reconstruction(Parents: np.ndarray):
     result = []
-    i = Parents.shape[0]
-    j = Parents.shape[1]
+    i = Parents.shape[0] - 1
+    j = Parents.shape[1] - 1
     curr = Parents[i][j]
     while curr != 'X':
 
@@ -75,11 +76,10 @@ def path_reconstruction(Parents: np.ndarray):
 
         result.append(curr)
         curr = Parents[i][j]
-        
-    return result.reverse()
 
-
-
+    result.reverse()
+    result = ''.join(result)
+    return result
 
 def main():
     P1 = ' kot'
@@ -90,10 +90,16 @@ def main():
 
     print(string_compare_recur(P1, T1, 3, 3))
     print(string_compare_recur(P2, T2, 3, 4))
-    min_cost, Parents = string_compare_PD(P1,T1,len(P1),len(T1))
+    min_cost, Parents = string_compare_PD(P1,T1,len(P1) - 1,len(T1) - 1)
     print(min_cost)
-    min_cost, Parents = string_compare_PD(P2,T2,len(P2),len(T2))
+    min_cost, Parents = string_compare_PD(P2,T2,len(P2) - 1,len(T2) - 1)
     print(min_cost)
 
-    path_reconstruction(Parents)
+    P3 = ' thou shalt not'
+    T3 = ' you should not'
+
+    min_cost, Parents = string_compare_PD(P3, T3, len(P3) - 1, len(T3) - 1)
+    print(path_reconstruction(Parents))
+
+
 main()
